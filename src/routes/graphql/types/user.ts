16268d9b67ace298c21e6dd411756f1169fc8339
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-11-23 17:25:06 +0100

    feat: add UserType GraphQL object type

---
+++ src/routes/graphql/types/user.ts
@@ -0,0 +1,31 @@
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLList,
+} from "graphql";
+import { UUIDType } from "./uuid.js";
+import { ProfileType } from "./profile.js";
+import { PostType } from "./post.js";
+
+export const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+
+    profile: { type: ProfileType },
+
+    posts: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType))) },
+
+    userSubscribedTo: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+    },
+
+    subscribedToUser: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+    },
+  }),
+});
