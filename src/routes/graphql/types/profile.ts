86a2ee8c402767b5f1f42aecd8eb22560d0a0c41
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-11-24 01:04:24 +0100

    feat: add generics and resolvers to ProfileType

--- src/routes/graphql/types/profile.ts
+++ src/routes/graphql/types/profile.ts
@@ -3,16 +3,20 @@ import {
   GraphQLNonNull,
   GraphQLBoolean,
   GraphQLInt,
-} from "graphql";
+  } from "graphql";
 import { UUIDType } from "./uuid.js";
 import { MemberTypeType } from "./memberType.js";
+import { GqlProfile } from "../gqlTypes.js";
 
-export const ProfileType = new GraphQLObjectType({
+export const ProfileType = new GraphQLObjectType<GqlProfile>({
   name: "Profile",
   fields: () => ({
     id: { type: new GraphQLNonNull(UUIDType) },
     isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
     yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
-    memberType: { type: new GraphQLNonNull(MemberTypeType) },
+    memberType: {
+      type: MemberTypeType,
+      resolve: (parent) => parent?.memberType ?? null,
+    },
   }),
 });
