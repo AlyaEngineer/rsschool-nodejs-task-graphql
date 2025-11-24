9c5fc71c967df7209e8f02777ee06577306311ff
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-11-24 01:38:55 +0100

    chore: add query depth limit to GraphQL endpoint using graphql-depth-limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, specifiedRules, validate, parse } from 'graphql';
 import { schema } from './types/schema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -16,6 +17,16 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const errors = validate(
+        schema,
+        parse(req.body.query),
+        [...specifiedRules, depthLimit(5)]
+      );
+
+      if (errors.length) {
+        return { errors };
+      }
+
       return graphql({
         schema,
         source: req.body.query,
