9225e8f64a201310d35372f01240ea495082ef05
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-11-24 01:03:09 +0100

    feat: add generics and resolvers to UserType

--- src/routes/graphql/types/user.ts
+++ src/routes/graphql/types/user.ts
@@ -8,24 +8,53 @@ import {
 import { UUIDType } from "./uuid.js";
 import { ProfileType } from "./profile.js";
 import { PostType } from "./post.js";
+import { GraphQLContext } from "./context.js";
+import { GqlUser } from "../gqlTypes.js";
 
-export const UserType = new GraphQLObjectType({
+export const UserType = new GraphQLObjectType<GqlUser, GraphQLContext>({
   name: "User",
   fields: () => ({
     id: { type: new GraphQLNonNull(UUIDType) },
     name: { type: new GraphQLNonNull(GraphQLString) },
     balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: {
+      type: ProfileType,
+      resolve: async (parent, _args, context) => {
+        if (!parent?.id) return null;
+        return context.prisma.profile.findUnique({
+          where: { userId: parent.id },
+          include: { memberType: true },
+        }) ?? null;
+      },
+    },
 
-    profile: { type: ProfileType },
-
-    posts: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType))) },
+    posts: {
+      type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(PostType))),
+      resolve: async (parent, _args, context) => {
+        return (await context.prisma.post.findMany({ where: { authorId: parent.id } })) ?? [];
+      },
+    },
 
     userSubscribedTo: {
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+      resolve: async (parent, _args, context) => {
+        const subscriptions = await context.prisma.subscribersOnAuthors.findMany({
+          where: { subscriberId: parent.id },
+          include: { author: true },
+        });
+        return subscriptions.map((s) => s.author);
+      },
     },
-
+    
     subscribedToUser: {
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))),
+      resolve: async (parent, _args, context) => {
+        const subscriptions = await context.prisma.subscribersOnAuthors.findMany({
+          where: { authorId: parent.id },
+          include: { subscriber: true },
+        });
+        return subscriptions.map((s) => s.subscriber);
+      },
     },
   }),
 });
